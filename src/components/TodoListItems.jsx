30585192ba078f27803172137fc95c28c755bc8f
cristianh <crishernandez08@hotmail.com>
2023-01-20 09:58:17 -0500

    UPDATE: agregando funcionalidad botones de la lista todo list

--- src/components/TodoListItems.jsx
+++ src/components/TodoListItems.jsx
@@ -1,22 +1,31 @@
 import React from 'react'
 
-export const TodoListItems = ({ todo }) => {
+export const TodoListItems = ({ todo, onCancelTodo, onDoneTodo, onUpdateTodo }) => {
     return (
         <>
-            <li key={todo.id} className="list-group-item">
+            <li className="list-group-item">
                 <div className="row">
-                    <div className="col-8 d-flex justify-content-center align-items-center">
-                        <span>
-                            {todo.descripcion}
-                        </span>
+                    <div className="col-8 d-flex justify-content-left align-items-center" >
+                        <div className="flex  justify-content-between align-content-start align-items-start p-3">
+                            <input className="form-check-input align-start mx-3" type="checkbox" onChange={() => onDoneTodo(todo.id)} name="checkItem" id="checkItem" />
+                            <span
+                                className={`${todo.estado ? 'text-decoration-line-through ' : ''}`}>
+                                {todo.descripcion}
+                            </span>
+                        </div>
+
                     </div>
                     <div className="col-4">
-                        <div class="d-grid gap-3">
-                            <button className="btn  btn-outline-danger">
-                                Cancelar
+                        <div className="d-grid gap-3">
+                            <button
+                                onClick={() => onCancelTodo(todo.id)}
+                                className="btn  btn-outline-danger">
+                                Eliminar
                             </button>
-                            <button className="btn btn-outline-info">
-                                Terminado
+                            <button
+                                onClick={() => onUpdateTodo(todo.id)}
+                                className="btn btn-outline-info">
+                                Editar
                             </button>
                         </div>
                     </div>
